021cba74b173793ad1ef8852a3065c3051f5d0cf
Parshant Kumar Singh <146373184+parsh05@users.noreply.github.com>
2024-08-24 18:38:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -50,19 +50,24 @@ class Solution {
         // your code here
         int n = wt.length;
         int[][] dp = new int[n + 1][W + 1];
-        for(int[] row : dp) Arrays.fill(row, -1);
         
         return solve(W, n, wt, val, dp);
     }
     private static int solve(int W,int n, int wt[], int val[], int[][]dp){
-        if( W <= 0 || n <= 0 ) return 0;
-        if( dp[n][W] != -1 ) return dp[n][W];
-        
-        if(wt[n-1] <= W){
-            dp[n][W] = Math.max(val[n-1] + solve(W-wt[n-1], n-1, wt, val, dp) , solve(W, n-1, wt, val, dp));
-        } else{
-            dp[n][W] = solve(W, n-1, wt, val, dp);
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= W; j++){
+                if(i ==0 || j == 0) dp[i][j] = 0;
+            }
         }
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= W; j++){
+                if(wt[i-1] <= j) 
+                    dp[i][j] = Math.max( val[i-1] + dp[i-1][j-wt[i-1]] , dp[i-1][j] );
+                else dp[i][j] = dp[i-1][j] ;
+            }
+        }
+        
         return dp[n][W];
     }
 }
