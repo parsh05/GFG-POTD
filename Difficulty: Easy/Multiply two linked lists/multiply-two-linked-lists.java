996de08631981140c88ca711e35bb170c6b776e4
Parshant Kumar Singh <146373184+parsh05@users.noreply.github.com>
2024-10-01 13:14:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Multiply two linked lists/multiply-two-linked-lists.java
+++ Difficulty: Easy/Multiply two linked lists/multiply-two-linked-lists.java
@@ -1,71 +1,22 @@
 //{ Driver Code Starts
 import java.util.*;
-class Node
-{
+
+class Node {
     int data;
     Node next;
-    Node(int data){
-        this.data=data;
-        next=null;
+
+    Node(int x) {
+        data = x;
+        next = null;
     }
 }
 
-class MultiplyTwoLL{
-	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		int t=sc.nextInt();
-		while(t-->0){
-			Node head=null;
-			Node phead=null;
-			int n=sc.nextInt();
-			while(n-->0){
-				int n1=sc.nextInt();
-				if(head==null)
-				head=new Node(n1);
-			else{
-				addHead(head,n1);
-			}
-			}
-			int m=sc.nextInt();
-			while(m-->0){
-				int n1=sc.nextInt();
-				if(phead==null)
-				phead=new Node(n1);
-			else{
-				addPhead(phead,n1);
-			}
-			}
-		GfG g=new GfG();
 
-System.out.println(g.multiplyTwoLists(head,phead));
-		}
-	}
-    public static void addHead(Node node,int a) {
-          if (node == null)
-            return;
-		if(node.next==null)
-			node.next=new Node(a);
-		else
-			addHead(node.next,a);
-	}
-	public static void addPhead(Node node,int a){
-		 if (node == null)
-            return;
-		if(node.next==null)
-		node.next=new Node(a);
-	else
-		addPhead(node.next,a);
-	}
-	}
-	
-	
 // } Driver Code Ends
-
-
 /*Node is defined as
 class Node
 {
-    int data; 
+    int data;
     Node next;
     Node(int data) {
         this.data=data;
@@ -73,24 +24,101 @@ class Node
     }
 }*/
 
-class GfG{
-  /*You are required to complete this method */
-   public long multiplyTwoLists(Node l1,Node l2){
-          //add code here.
-          Node t1 = l1, t2 = l2;
-          long mod = 1000000007;
-          long sum1 = 0, sum2 = 0;
-          while( t1!= null || t2 != null){
-              if( t1 != null ) {
-                  sum1 = ((sum1 * 10 )%mod + t1.data) % mod;
-                  t1 = t1.next;
-              }
-              if( t2 != null ){
-                  sum2 = ((sum2 * 10 )%mod + t2.data) % mod;
-                  t2 = t2.next;
-              }
-          }
-          
-          return ( sum1 * sum2 ) % mod;
-   }
+class Solution {
+    public long multiplyTwoLists(Node first, Node second) {
+        // Code here
+        long num1 = 0, num2 = 0;
+        long mod = 1000000007;
+        Node t = first;
+        while(t != null ){
+            num1 = (num1 * 10 + t.data) % mod ;
+            t = t.next;
+        }
+        t = second;
+        while(t != null ){
+            num2 = (num2 * 10 + t.data) % mod;
+            t = t.next;
+        }
+        return (num1 * num2) % 1000000007;
+    }
 }
+
+//{ Driver Code Starts.
+
+public class GFG {
+    public static Node newNode(int data) { return new Node(data); }
+
+    public static void push(Node[] head_ref, int new_data) {
+        Node new_Node = newNode(new_data);
+        new_Node.next = head_ref[0];
+        head_ref[0] = new_Node;
+    }
+
+    public static void reverse(Node[] r) {
+        Node prev = null;
+        Node cur = r[0];
+        Node nxt;
+        while (cur != null) {
+            nxt = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = nxt;
+        }
+        r[0] = prev;
+    }
+
+    public static void freeList(Node Node) {
+        Node temp;
+        while (Node != null) {
+            temp = Node;
+            Node = Node.next;
+            // In Java, garbage collection takes care of freeing memory.
+        }
+    }
+
+    public static void printList(Node Node) {
+        while (Node != null) {
+            System.out.print(Node.data + " ");
+            Node = Node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+
+        while (t-- > 0) {
+            Node[] first = new Node[1];
+            Node[] second = new Node[1];
+            first[0] = null;
+            second[0] = null;
+
+            String input = sc.nextLine();
+            String[] arr = input.split(" ");
+            for (String s : arr) {
+                push(first, Integer.parseInt(s));
+            }
+
+            String input2 = sc.nextLine();
+            String[] brr = input2.split(" ");
+            for (String s : brr) {
+                push(second, Integer.parseInt(s));
+            }
+
+            reverse(first);
+            reverse(second);
+
+            Solution ob = new Solution();
+            long res = ob.multiplyTwoLists(first[0], second[0]);
+            System.out.println(res);
+
+            // Free list is not necessary in Java
+        }
+
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
