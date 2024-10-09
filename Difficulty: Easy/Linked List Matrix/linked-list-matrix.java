860b5f3a7e91e7052143b65d43ddd3199c470152
Parshant Kumar Singh <146373184+parsh05@users.noreply.github.com>
2024-10-09 17:49:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Linked List Matrix/linked-list-matrix.java
+++ Difficulty: Easy/Linked List Matrix/linked-list-matrix.java
@@ -103,27 +103,54 @@ class Node
    }
 }
 */
+/*class Node
+
+class Node
+{
+   int data;
+   Node right,down;
+
+   Node(int data){
+       this.data = data;
+       right = null;
+       down = null;
+   }
+}
+*/
 /*Function should return the head of the 2D LL.*/
 class Solution {
-    // Method to construct a linked list from a 2D array
-    static Node constructUtil(int arr[][], int i, int j, int m) {
-        if (i >= m || j >= m) return null;
-
-        // Create a new node with data from the 2D array
-        Node temp = new Node(arr[i][j]);
-
-        // Recursively construct the right and down pointers
-        temp.right = constructUtil(arr, i, j + 1, m);
-        temp.down = constructUtil(arr, i + 1, j, m);
-
-        // Return the constructed node
-        return temp;
-    }
-
-    // Function to construct the linked list from a 2D array
     static Node construct(int arr[][]) {
-        int m = arr.length;
+        int n = arr.length;
         
-        return constructUtil(arr, 0, 0, m);
+        if(n==0){
+            return null;
+        }
+        
+        Node head = new Node(arr[0][0]);
+        Node[] prevRow = new Node[n];
+        prevRow[0] = head;
+        
+        Node current = head;
+        for(int j=1; j<n; j++){
+            current.right = new Node(arr[0][j]);
+            current = current.right;
+            prevRow[j] = current;
+        }
+        
+        for(int i=1; i<n; i++){
+            Node newRowHead = new Node(arr[i][0]);
+            prevRow[0].down = newRowHead;
+            prevRow[0] = newRowHead;
+
+            current = newRowHead;
+            for (int j = 1; j < n; j++) {
+                current.right = new Node(arr[i][j]);
+                prevRow[j].down = current.right;
+                prevRow[j] = current.right;
+                current = current.right;
+            }
+        }
+        
+        return head;
     }
 }
