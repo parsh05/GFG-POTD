d3e7efb018bab4c9c80a736fcd62c7ebe5ad2730
Parshant Kumar Singh <146373184+parsh05@users.noreply.github.com>
2024-10-15 11:20:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarray range with given sum/subarray-range-with-given-sum.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    // Function to count the number of subarrays which adds to the given sum.
+    static int subArraySum(int arr[], int tar) {
+        // add your code here
+        int n = arr.length;
+        for(int i =1;i < n;i++){
+            arr[i] += arr[i-1];
+        }
+        int count = 0;
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        for(int i = 0; i < n;i++){
+            if(arr[i] == tar) count++;
+            count+= mp.getOrDefault( arr[i] - tar, 0);
+            mp.put(arr[i], mp.getOrDefault(arr[i], 0) + 1);
+        }
+        return count;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int tar = Integer.parseInt(br.readLine());
+            Solution obj = new Solution();
+            int res = obj.subArraySum(arr, tar);
+
+            System.out.println(res);
+            // System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
