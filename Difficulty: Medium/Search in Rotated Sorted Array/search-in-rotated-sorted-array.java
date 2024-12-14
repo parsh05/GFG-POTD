f00a80797be43a67ab33e03693ff98d538806d9b
Parshant Kumar Singh <146373184+parsh05@users.noreply.github.com>
2024-12-14 10:07:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
@@ -29,6 +29,8 @@ public class GFG {
             int key = Integer.parseInt(in.readLine().trim());
 
             out.println(new Solution().search(arr, key));
+
+            out.println("~");
         }
         out.close();
     }
@@ -41,40 +43,19 @@ public class GFG {
 class Solution {
     int search(int[] arr, int key) {
         // Complete this function
-        int left=0;
-        int right=arr.length-1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            // Check if the middle element is the key
-            if (arr[mid] == key) {
-                return mid;
-            }
-            
-            // Check if the left half is sorted
-            if (arr[left] <= arr[mid]) {
-                // Check if the key is in the left half
-                if (key >= arr[left] && key < arr[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
-            // Otherwise, the right half must be sorted
-            else {
-                // Check if the key is in the right half
-                if (key > arr[mid] && key <= arr[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+        int ans = -1;
+        int l = 0, h = arr.length - 1;
+        while(l <= h){
+            int mid = (l + h)/2;
+            if(key == arr[mid]) return ans = mid;
+            if(arr[l] <= arr[mid]){
+                if(key >= arr[l] && key <= arr[mid]) h = mid - 1;
+                else l = mid + 1;
+            }else{
+                if(key >= arr[mid] && key <= arr[h]) l = mid + 1;
+                else h = mid - 1;
             }
         }
-        
-        // If the element is not found
-        return -1;
+        return ans;
     }
 }
-
- 
